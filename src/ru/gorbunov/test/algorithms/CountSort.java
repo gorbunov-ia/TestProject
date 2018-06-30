367f2810ef8d342b7a7d4e14312b39ab1242dadb
gorbunov-ia <Grbnv.vn@gmail.com>
2018-06-30 11:16:51 +0300

    Add example for count sort.

---
+++ src/ru/gorbunov/test/algorithms/CountSort.java
@@ -0,0 +1,53 @@
+package ru.gorbunov.test.algorithms;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class CountSort {
+
+    private static final int MAX_DIGIT = 10;
+
+    public static void main(String[] args) {
+        final int[] source = getArray();
+        final int[] elements = getElements();
+
+        for (int digit : source) {
+            elements[digit - 1] = ++elements[digit - 1];
+        }
+
+        for (int i = 1; i < elements.length; i++) {
+            elements[i] = elements[i - 1] + elements[i];
+        }
+
+        final int[] result = new int[source.length];
+        for (int i = source.length - 1; i >= 0; i--) {
+            int index = source[i] - 1;
+            result[elements[index] - 1] = source[i];
+            elements[index] = --elements[index];
+        }
+
+        StringBuilder builder = new StringBuilder(result.length);
+        builder.append(result[0]);
+        for (int i = 1; i < result.length; i++) {
+            builder.append(" ").append(result[i]);
+        }
+        System.out.println(builder.toString());
+    }
+
+    private static int[] getElements() {
+        return new int[MAX_DIGIT];
+    }
+
+    private static int[] getArray() {
+        Scanner scanner = new Scanner(new ByteArrayInputStream("5\n2 3 9 2 9".getBytes()));
+//        Scanner scanner = new Scanner(System.in);
+        final int length = scanner.nextInt();
+        final int[] source = new int[length];
+        for (int i = 0; i < source.length; i++) {
+            source[i] = scanner.nextInt();
+        }
+        return source;
+    }
+
+
+}
